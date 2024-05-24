67e7915942e8c3516b11aec336f703a39b3920af
Moenul <moenul22@gmail.com>
2024-05-24 10:54:57 +0600

    First router with navigation successfully created

--- src/main.js
+++ src/main.js
@@ -3,10 +3,23 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
+import viewNotes from "@/views/viewNotes.vue";
+import viewStats from "@/views/viewStats.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "notes",
+      component: viewNotes,
+    },
+    {
+      path: "/stats",
+      name: "stats",
+      component: viewStats,
+    },
+  ],
 });
 
 createApp(App).use(router).mount("#app");
